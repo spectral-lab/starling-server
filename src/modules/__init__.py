2bac3ebd70d72efb6acaf51e6717972374176801
szk2s <suzukisjss@gmail.com>
2019-04-15 21:02:10 +0900

    Add two module into index
    - plot
    - compute_marks

--- src/modules/__init__.py
+++ src/modules/__init__.py
@@ -1,5 +1,7 @@
 from .detect_peaks import detect_peaks
 from .segmentize import segmentize
 from .format_check import format_check
+from .plot import plot
+from .compute_marks import compute_marks
 
 __all__ = [s for s in dir() if not s.startswith('_')]
