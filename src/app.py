195bcc0b1ffb6362114f3917679f93fc8728bffe
szk2s <suzukisjss@gmail.com>
2019-04-17 21:13:10 +0900

    rewrite handler

--- src/app.py
+++ src/app.py
@@ -1,8 +1,8 @@
 import sys
 import os
 
-__dirname = os.path.dirname(__file__)
-sys.path.append(os.path.normpath(os.path.join(__dirname, "/modules")))
+__dirname__ = os.path.dirname(__file__)
+sys.path.append(os.path.normpath(os.path.join(__dirname__, "/modules")))
 
 PACKAGE_PARENT = '..'
 from PIL import Image
@@ -10,41 +10,50 @@ from skimage import img_as_float
 import io
 from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS
-from modules import segmentize, format_check, detect_peaks
-from librosa.core import db_to_power
-import numpy as np
-print('Successfully imported')
+import datetime
+# noinspection PyPackageRequirements,PyUnresolvedReferences
+from modules import segmentize, check_format, detect_peaks, compute_marks, export_graph, format_as_2d_array
+import pytz
+
+print('Success: Imported modules')
 
 app = Flask(__name__)
 CORS(app)
+line_continuity = 1  # This will be taken from the request from client
 
 
 @app.route('/', methods=["POST"])
 def handler():
-    line_continuity = 1
-    peak_level = db_to_power(-30)
-    background_level = db_to_power(-35)
-    print(peak_level, background_level)
+    def export_intermediate_data():
+        peaks_2d = format_as_2d_array(peak_points, spectrogram.shape)
+        now = datetime.datetime.now(pytz.timezone('Asia/Tokyo'))
+        export_graph(spectrogram,
+                     "spectrogram_" + now.strftime("%Y%m%d_%H%M"))  # ex: spectrogram_20190417_0910
+        export_graph(markers, "markers_" + now.strftime("%Y%m%d_%H%M"))
+        export_graph(labels, "labels_" + now.strftime("%Y%m%d_%H%M"))
+        export_graph(peaks_2d, "peak_points_" + now.strftime("%Y%m%d_%H%M"))
+        print("Success: Exported graphs")
+
     print('Success: Got request')
     uploaded_img = request.data
     image_data = Image.open(io.BytesIO(uploaded_img)).convert('L')
-    img = img_as_float(image_data) / 255.
-    # np.save('./src/modules/test/data/img_from_client/era.npy', img)
-    check_result = format_check(img)
+    spectrogram = img_as_float(image_data) / 255.
+    check_result = check_format(spectrogram)
     if not check_result["is_ok"]:
         print("bad format")
-        print(check_result)
+        print(check_result['msg'])
         return check_result['msg']
     print('Success: Converted into ndarray')
-    labels = segmentize(img, line_continuity, peak_level, background_level)
+    markers = compute_marks(spectrogram)
+    labels = segmentize(spectrogram, markers, line_continuity)
     print('Success: Segmentized with ' + str(labels.max() + 1) + " segments to extract peaks")
-    peak_points = detect_peaks(img, labels)
+    peak_points = detect_peaks(spectrogram, labels)
     print('Success: Detected ' + str(len(peak_points)) + ' peaks')
-    ret = make_response(convert_to_json(peak_points))
+    ret = make_response(jsonify(peak_points))
+
+    export_intermediate_data()  # Optional
+
     print('Success: Returns')
     print(str(peak_points))
     return ret
 
-
-def convert_to_json(in_list):
-    return jsonify(in_list)
