4f6acb3613f4037c5c7d0575f28cbf8bd1a886fb
szk2s <suzukisjss@gmail.com>
2019-04-22 22:28:39 +0900

    Adjust default parameter

--- src/modules/test/test_overall_process.py
+++ src/modules/test/test_overall_process.py
@@ -10,8 +10,10 @@ import os
 
 __dirname__ = os.path.dirname(__file__)
 
-line_continuity = 1  # This will be taken from the request from client
-
+line_continuity = 0  # This will be taken from the request from client
+proportion_of_hottest_area = 0.01   # This will be taken from the request from client
+proportion_of_coldest_area = 0.5   # This will be taken from the request from client
+degree = 6  # This will be taken from the request from client
 
 class TestOverallProcess(TestCase):
     def test_overall_process(self):
@@ -23,8 +25,7 @@ class TestOverallProcess(TestCase):
                              filename + "_spectrogram")  # ex: spectrogram_20190417_0910
                 export_graph(markers, filename + "_markers")
                 export_graph(segment_labels, filename + "_segment_labels")
-                export_graph(feature_lines_to_image(feature_lines, spectrogram.shape),
-                             filename + "_feature_lines")
+                export_graph(feature_lines_to_image(feature_lines, spectrogram.shape), filename + "_feature_lines")
 
             spectrogram = np.load(filepath)
             check_result = check_format(spectrogram)
@@ -32,10 +33,10 @@ class TestOverallProcess(TestCase):
                 print("bad format")
                 print(check_result['msg'])
                 return check_result['msg']
-            markers = compute_seeds(spectrogram)
+            markers = compute_seeds(spectrogram, proportion_of_hottest_area, proportion_of_coldest_area)
             segment_labels = segmentize(spectrogram, markers, line_continuity)
             training_points = extract_training_points(segment_labels, spectrogram, ratio=0.3)
-            feature_lines = compute_feature_lines(training_points, degree=6)
+            feature_lines = compute_feature_lines(training_points, degree)
 
             export_intermediate_data_as_graph()  # Optional
 
